05a27d8cba4d9db30934e6672c41c5775a45cc39
Valentin Tanasi <valytns@gmail.com>
2023-10-17 19:33:32 +0300

    import rtd theme

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,8 @@ author = 'Valentin'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import sphinx_rtd_theme
+
 extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.duration",
